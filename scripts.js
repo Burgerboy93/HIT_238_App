028d4fa06f02621d66a4348f765d8426b256cacb
Burgerboy93 <32314803+Burgerboy93@users.noreply.github.com>
2018-09-21 18:57:22 +0930

    Sprint 1 first upload
    first commit of the first sprint, making application better suited for mobile devices

--- scripts.js
+++ scripts.js
@@ -1,50 +1,56 @@
-function beginLearning(){
-  var toggle = document.getElementsByClassName('container');
-  toggle[0].classList.remove("active");
-  toggle[1].classList.add("active");
-};
-
-function previousLesson(){
-  var toggle = document.getElementsByClassName('container');
-  toggle[2].classList.remove("active");
-  toggle[1].classList.add("active");
-};
-
-function nextLesson(){
-  var toggle = document.getElementsByClassName('container');
-  toggle[1].classList.remove("active");
-  toggle[2].classList.add("active");
-};
-
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
        var response = JSON.parse(xhttp.responseText);
        console.log(response)
-       var word = response.word;
-       var definition = response.definition;
-       var sentences = response.sentences;
-       var sentenceDefinition = response.sentenceDefinition;
 
+       var japaneseWords = response.japaneseWord;
+       var japaneseDefinitions = response.japaneseDefinition;
+       var germanWords = response.germanWord;
+       var germanDefinitions = response.germanDefinition;
+       var danishWords = response.danishWord;
+       var danishDefinitions = response.danishDefinition;
 
-       var greetingSentences = '';
-       var greetingWordContent = '';
+       var japaneseWordContent = '';
+       var germanWordContent = '';
+       var danishWordContent = '';
 
-       for (var i = 0; i < word.length; i++) {
-
-         greetingWordContent += '<tr><td>' + word[i]+ '</td><td>' + definition[i] + '</td></tr>';
-
-
+       for (var i = 0; i < japaneseWords.length; i++) {
+         japaneseWordContent += '<tr><td>' + japaneseWords[i]+ '</td><td>' + japaneseDefinitions[i] + '</td></tr>';
        };
-       for (var i = 0; i < sentences.length; i++){
-       greetingSentences += '<tr><td>' + sentences[i]+ '</td><td>' + sentenceDefinition[i] + '</td></tr>';
-     }
-       document.getElementById('greetingContent').innerHTML = greetingWordContent;
-       document.getElementById('greetingSentencesContent').innerHTML = greetingSentences;
 
+       for (var i = 0; i < germanWords.length; i++) {
+         germanWordContent += '<tr><td>' + germanWords[i]+ '</td><td>' + germanDefinitions[i] + '</td></tr>';
+       };
+
+       for (var i = 0; i < danishWords.length; i++) {
+         danishWordContent += '<tr><td>' + danishWords[i]+ '</td><td>' + danishDefinitions[i] + '</td></tr>';
+       };
+       document.getElementById('japaneseContent').innerHTML = japaneseWordContent;
+       document.getElementById('germanContent').innerHTML = germanWordContent;
+       document.getElementById('danishContent').innerHTML = danishWordContent;
     };
 };
 
 
 xhttp.open("GET", "lessons.json", true);
 xhttp.send();
+
+
+function learnJapanese(){
+  var toggle = document.getElementsByClassName('panel');
+  toggle[0].classList.remove("active");
+  toggle[1].classList.add("active");
+};
+
+function learnGerman(){
+  var toggle = document.getElementsByClassName('panel');
+  toggle[0].classList.remove("active");
+  toggle[2].classList.add("active");
+};
+
+function learnDanish(){
+  var toggle = document.getElementsByClassName('panel');
+  toggle[0].classList.remove("active");
+  toggle[3].classList.add("active");
+};
