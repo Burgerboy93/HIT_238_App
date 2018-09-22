59747d98c8deae8c79c451859c519e83a2873501
Burgerboy93 <32314803+Burgerboy93@users.noreply.github.com>
2018-09-22 21:18:39 +0930

    Second Commit
    Commit to test layout of content.

--- scripts.js
+++ scripts.js
@@ -6,51 +6,24 @@ xhttp.onreadystatechange = function() {
 
        var japaneseWords = response.japaneseWord;
        var japaneseDefinitions = response.japaneseDefinition;
-       var germanWords = response.germanWord;
-       var germanDefinitions = response.germanDefinition;
-       var danishWords = response.danishWord;
-       var danishDefinitions = response.danishDefinition;
+
 
        var japaneseWordContent = '';
-       var germanWordContent = '';
-       var danishWordContent = '';
+       var japaneseWordDefinitions = '';
 
        for (var i = 0; i < japaneseWords.length; i++) {
-         japaneseWordContent += '<tr><td>' + japaneseWords[i]+ '</td><td>' + japaneseDefinitions[i] + '</td></tr>';
+         japaneseWordContent += '<div class="tab-btn"><button class="contentButtons"><em class="mdi mdi-plus"></em>' + japaneseWords[i]+ '</button></div>';
+       };
+       for (var i = 0; i < japaneseDefinitions.length; i++) {
+         japaneseWordDefinitions += '<div class="tab-content"><p>'+ japaneseDefinitions[i]+ '</p></div>';
+
        };
 
-       for (var i = 0; i < germanWords.length; i++) {
-         germanWordContent += '<tr><td>' + germanWords[i]+ '</td><td>' + germanDefinitions[i] + '</td></tr>';
-       };
-
-       for (var i = 0; i < danishWords.length; i++) {
-         danishWordContent += '<tr><td>' + danishWords[i]+ '</td><td>' + danishDefinitions[i] + '</td></tr>';
-       };
-       document.getElementById('japaneseContent').innerHTML = japaneseWordContent;
-       document.getElementById('germanContent').innerHTML = germanWordContent;
-       document.getElementById('danishContent').innerHTML = danishWordContent;
+       document.getElementById('japaneseWords').innerHTML = japaneseWordContent;
+       document.getElementById('japaneseDefinitions').innerHTML = japaneseWordDefinitions;
     };
 };
 
 
 xhttp.open("GET", "lessons.json", true);
 xhttp.send();
-
-
-function learnJapanese(){
-  var toggle = document.getElementsByClassName('panel');
-  toggle[0].classList.remove("active");
-  toggle[1].classList.add("active");
-};
-
-function learnGerman(){
-  var toggle = document.getElementsByClassName('panel');
-  toggle[0].classList.remove("active");
-  toggle[2].classList.add("active");
-};
-
-function learnDanish(){
-  var toggle = document.getElementsByClassName('panel');
-  toggle[0].classList.remove("active");
-  toggle[3].classList.add("active");
-};
